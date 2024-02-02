0f61c13377cc2d027d95f7a23714e0a4bf3462d8
NITISH KUMAR <nitishkumar332921@gmail.com>
2024-02-02 19:46:38 +0530

    Added about us page and fixed sidebar login signup button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css';
 
 import { Route, Routes } from 'react-router-dom';
+
+import AboutUs from './Pages/AboutUs';
 import HomePage from './Pages/HomePage';
 
 function App() {
-
   return (
     <>
-    <Routes>
-       <Route path="/" element={<HomePage />} ></Route>
+      <Routes>
+        <Route path="/" element={<HomePage />} ></Route>
+        <Route path="/about" element={<AboutUs />} ></Route>
 
-    </Routes>
+      </Routes>
     </>
   )
 }
-
 export default App
+ 
